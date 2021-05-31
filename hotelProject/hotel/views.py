3e4a6a5e148c2cec8240bda1648230d8f4b2c52d
Bef <thitiworada.ams@mail.kmutt.ac.th>
2021-06-01 03:42:36 +0700

    move fronend

--- hotelProject/hotel/views.py
+++ hotelProject/hotel/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def dinning(request):
+    return render(request,'rest.html')
+
+def room(request):
+    return render(request,'room.html')
+
+def promotion(request):
+    return render(request,'promotion.html')
+
+def contact(request):
+    return render(request,'contact.html')
+

df842de1bfb4d6248b1b7785907436e34df9a76a
Bef <thitiworada.ams@mail.kmutt.ac.th>
2021-06-01 07:02:38 +0700

    add url

--- hotelProject/hotelProject/urls.py
+++ hotelProject/hotelProject/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('promotions/',views.promotion, name='promotion'),
     path('contact/',views.contact, name='contact'),
     path('login/',views.login,name='login'),
+    path('loginaccept/',views.logincheck,name='login'),
     path('book_room/',views.bookroom,name='bookroom'),
     path('book_table/',views.bookrest,name='bookrest'),
     path('logout/',auth.LogoutView.as_view(template_name= 'index.html'),name='logout')

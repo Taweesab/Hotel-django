76c2adb9909260422bbfb63ab9eb68a95e27e0c0
onwp <onwipa.3108@mail.kmutt.ac.th>
2021-06-02 22:40:13 +0700

    admin

--- hotelProject/hotel/admin.py
+++ hotelProject/hotel/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import promotion_type
+from .models import *
 
 # Register your models here.
 admin.site.register(promotion_type)
+admin.site.register(room_type)
+admin.site.register(service)
+admin.site.register(buffet_round)

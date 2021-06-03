3848cb7882f8d2fdee94269cf2969f60b254120a
Bef <thitiworada.ams@mail.kmutt.ac.th>
2021-06-03 23:43:45 +0700

    edit name model\

--- hotelProject/hotel/admin.py
+++ hotelProject/hotel/admin.py
@@ -3,19 +3,19 @@ from .models import *
 
 # Register your models here.
 #### try model #########
-admin.site.register(staff)
-admin.site.register(customer)
-admin.site.register(customer_booking)
-admin.site.register(promotion_type)
-admin.site.register(room_booking)
-admin.site.register(room_type)
-admin.site.register(room_available)
-admin.site.register(service)
-admin.site.register(room_detail)
-admin.site.register(resbooking)
-admin.site.register(buffet_round)
-admin.site.register(buffet_table)
-admin.site.register(resb_detail)
-admin.site.register(invoice)
+admin.site.register(Staff)
+admin.site.register(Customer)
+admin.site.register(Customer_booking)
+admin.site.register(Promotion_type)
+admin.site.register(Room_booking)
+admin.site.register(Room_type)
+admin.site.register(Room_available)
+admin.site.register(Service)
+admin.site.register(Room_detail)
+admin.site.register(Resbooking)
+admin.site.register(Buffet_round)
+admin.site.register(Buffet_table)
+admin.site.register(Resb_detail)
+admin.site.register(Invoice)
 
 
--- hotelProject/hotel/models.py
+++ hotelProject/hotel/models.py
@@ -2,9 +2,9 @@ from os import name
 from django.db import models
 
 # Create your models here.
-class staff(models.Model):
+class Staff(models.Model):
     def genID():
-        n = staff.objects.count()
+        n = Staff.objects.count()
         if n == 0:
             return "STF00000001"
         else:
@@ -19,9 +19,9 @@ class staff(models.Model):
     email = models.EmailField(null=False,unique=True)
     password = models.CharField(max_length=100, null=False)
 
-class customer(models.Model):
+class Customer(models.Model):
     def genID():
-        n = customer.objects.count()
+        n = Customer.objects.count()
         if n == 0:
             return "CST00000001"
         else:
@@ -38,7 +38,7 @@ class customer(models.Model):
     def fullName(self):
         return self.fName + " " + self.lName
 
-class promotion_type(models.Model):
+class Promotion_type(models.Model):
     promotion_code = models.CharField(max_length=7, null=False, primary_key=True)
     promotion_name = models.CharField(max_length=32, null=False)
     promotion_detail = models.CharField(max_length=500, null = False)
@@ -49,18 +49,18 @@ class promotion_type(models.Model):
     def __str__(self) :
         return self.promotion_name
 
-class room_booking(models.Model):
+class Room_booking(models.Model):
     def bhID():
-        n = room_booking.objects.count()
+        n = Room_booking.objects.count()
         if n == 0:
             return "BH00000001"
         else:
             return "BH" + str(n+1).zfill(9)
     booking_no = models.CharField(max_length=11,default=bhID, null=False, primary_key=True)
-    staff_id = models.ForeignKey(staff, on_delete=models.CASCADE, null=False)
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE, null=False)
     date_check_in = models.DateTimeField(null = False)
     date_check_out = models.DateTimeField(null=False)
-    promotion_code = models.ForeignKey(promotion_type, on_delete=models.SET_NULL, null=True)
+    promotion_code = models.ForeignKey(Promotion_type, on_delete=models.SET_NULL, null=True)
     number_guest = models.IntegerField(null=False)
     total_charge = models.FloatField(null=False)
     payment_method = models.CharField(max_length=32)
@@ -69,7 +69,7 @@ class room_booking(models.Model):
     def __str__(self) :
         return self.date_check_in
 
-class room_type(models.Model):
+class Room_type(models.Model):
     roomtype = models.CharField(max_length=30, null=False, primary_key=True)
     capacity = models.IntegerField(null = False)
     price = models.FloatField(null=False)
@@ -79,12 +79,12 @@ class room_type(models.Model):
         return self.roomtype
 
 
-class room_available(models.Model):
+class Room_available(models.Model):
     room_no = models.CharField(max_length=4,null=False, primary_key=True)
-    roomtype = models.ForeignKey(room_type, on_delete=models.CASCADE, null=False)
+    roomtype = models.ForeignKey(Room_type, on_delete=models.CASCADE, null=False)
     status = models.BooleanField(null=False)
 
-class service(models.Model):
+class Service(models.Model):
     service_code = models.CharField(max_length=10, null=False, primary_key=True)
     service_name = models.CharField(max_length=20, null = False,unique=True)
     charge = models.FloatField(null = True)
@@ -94,51 +94,51 @@ class service(models.Model):
         return self.service_name
     
 
-class room_detail(models.Model):
-    booking_no = models.ForeignKey(room_booking, on_delete=models.RESTRICT, null=False)
-    room_no = models.ForeignKey(room_available, on_delete=models.CASCADE, null=False)
-    service_code = models.ForeignKey(service, on_delete = models.CASCADE, null=True)
+class Room_detail(models.Model):
+    booking_no = models.ForeignKey(Room_booking, on_delete=models.RESTRICT, null=False)
+    room_no = models.ForeignKey(Room_available, on_delete=models.CASCADE, null=False)
+    service_code = models.ForeignKey(Service, on_delete = models.CASCADE, null=True)
     service_count = models.IntegerField(null=False)
     
-class resbooking(models.Model):
+class Resbooking(models.Model):
     def brID():
-        n = resbooking.objects.count()
+        n = Resbooking.objects.count()
         if n == 0:
             return "BR00000001"
         else:
             return "BR" + str(n+1).zfill(9)
 
     resb_no = models.CharField(max_length=11,default= brID ,null=False,unique=True, primary_key=True)
-    staff_id = models.ForeignKey(staff, on_delete=models.CASCADE, null=False)
-    promotion_code = models.ForeignKey(promotion_type, on_delete=models.SET_NULL, null=True)
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE, null=False)
+    promotion_code = models.ForeignKey(Promotion_type, on_delete=models.SET_NULL, null=True)
     number_guest = models.IntegerField(null=False)
     total_charge = models.FloatField(null=False)
     payment_method = models.CharField(max_length=32)
 
-class buffet_round(models.Model):
+class Buffet_round(models.Model):
     round=models.CharField(max_length=64, null=False)
     charge=models.FloatField(null =True)
 
-class buffet_table(models.Model):
+class Buffet_table(models.Model):
     table_no = models.IntegerField(null=False)
-    round = models.ForeignKey(buffet_round, on_delete=models.CASCADE, null=False)
+    round = models.ForeignKey(Buffet_round, on_delete=models.CASCADE, null=False)
     status = models.BooleanField(null = False)
 
-class resb_detail(models.Model):
-    resb_no = models.ForeignKey(resbooking, on_delete=models.RESTRICT, null=False)
-    table_no = models.ForeignKey(buffet_table, on_delete=models.CASCADE, null=False)
+class Resb_detail(models.Model):
+    resb_no = models.ForeignKey(Resbooking, on_delete=models.RESTRICT, null=False)
+    table_no = models.ForeignKey(Buffet_table, on_delete=models.CASCADE, null=False)
 
-class customer_booking(models.Model):   
-    customer_id = models.ForeignKey(customer, on_delete=models.RESTRICT, null=False)
-    booking_no = models.ForeignKey(room_booking,on_delete=models.RESTRICT, null=True)
-    resb_no = models.ForeignKey(resbooking, on_delete=models.RESTRICT, null=True)
+class Customer_booking(models.Model):   
+    customer_id = models.ForeignKey(Customer, on_delete=models.RESTRICT, null=False)
+    booking_no = models.ForeignKey(Room_booking,on_delete=models.RESTRICT, null=True)
+    resb_no = models.ForeignKey(Resbooking, on_delete=models.RESTRICT, null=True)
     booking_date = models.DateTimeField(auto_now_add=True, null=False)
 
-class invoice(models.Model):
+class Invoice(models.Model):
     invoice_no = models.CharField(max_length=12, null=False, primary_key=True, unique=True)
-    booking_no = models.ForeignKey(room_booking, on_delete=models.RESTRICT, null=True)
-    resb_no = models.ForeignKey(resbooking, on_delete = models.RESTRICT, null=True)
-    customer_id = models.ForeignKey(customer, on_delete=models.RESTRICT, null=False)
-    staff_id = models.ForeignKey(staff, on_delete=models.CASCADE, null=False)
+    booking_no = models.ForeignKey(Room_booking, on_delete=models.RESTRICT, null=True)
+    resb_no = models.ForeignKey(Resbooking, on_delete = models.RESTRICT, null=True)
+    customer_id = models.ForeignKey(Customer, on_delete=models.RESTRICT, null=False)
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE, null=False)
     tax = models.FloatField(null=False)
     date = models.DateField(null=False)

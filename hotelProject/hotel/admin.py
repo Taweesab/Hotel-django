5686afb2b4ead41fb8f8b59e57f63b083ed96204
onwp <onwipa.3108@mail.kmutt.ac.th>
2021-06-05 20:32:30 +0700

    admin panel

--- hotelProject/hotel/admin.py
+++ hotelProject/hotel/admin.py
@@ -3,16 +3,60 @@ from .models import *
 
 # Register your models here.
 #### try model #########
-admin.site.register(Staff)
-admin.site.register(Customer)
-admin.site.register(Promotion_type)
-admin.site.register(Room)
-admin.site.register(Service)
-admin.site.register(Room_detail)
-admin.site.register(Room_booking)
-admin.site.register(Resbooking)
-admin.site.register(Buffet_round)
-admin.site.register(Customer_booking)
+class Admin_Staff(admin.ModelAdmin):
+    list_display=["staff_id","fname","lname","job_title"]
+    search_fields=["staff_id","fname","lname","job_title"]
+    list_filter=["job_title"]
+
+class Admin_Customer(admin.ModelAdmin):
+    list_display=["customer_id","fname","lname"]
+    search_fields=["fname","lname"]
+
+class Admin_Promo(admin.ModelAdmin):
+    list_display=["promotion_code","promotion_name","start_date","expire_date"]
+    search_fields=["promotion_code","promotion_name"]
+
+class Admin_Room(admin.ModelAdmin):
+    list_display=["roomtype","capacity","price"]
+    list_filter=["roomtype"]
+
+class Admin_Service(admin.ModelAdmin):
+    list_display=["service_code","service_name","charge"]
+
+class Admin_RoomDe(admin.ModelAdmin):
+    list_display=["detail_no","roomtype","service_code","room_count"]
+
+class Admin_BH(admin.ModelAdmin):
+    list_display=["booking_no","date_check_in","date_check_out","number_guest"]
+    search_fields=["booking_no"]
+
+class  Admin_Buffet(admin.ModelAdmin):
+    list_display=["buffet_round","charge","amount"]
+
+class Admin_BR(admin.ModelAdmin):
+    list_display=["resb_no","number_guest","eatdate","buffet_round"]
+    search_fields=["resb_no"]
+
+class Admin_CusBook(admin.ModelAdmin):
+    list_display=["customer_id","booking_no","resb_no","booking_date"]
+    list_filter=["customer_id"]
+
+class Admin_Invoice(admin.ModelAdmin):
+    list_display=["invoice_no","customer_id","staff_id","tax","date"]
+    list_filter=["staff_id"]
+    search_fields=["invoice_no","customer_id","staff_id"]
+
+admin.site.register(Staff,Admin_Staff)
+admin.site.register(Customer,Admin_Customer)
+admin.site.register(Promotion_type,Admin_Promo)
+admin.site.register(Room,Admin_Room)
+admin.site.register(Service,Admin_Service)
+admin.site.register(Room_detail,Admin_RoomDe)
+admin.site.register(Room_booking,Admin_BH)
+admin.site.register(Resbooking,Admin_BR)
+admin.site.register(Buffet_round,Admin_Buffet)
+admin.site.register(Customer_booking,Admin_CusBook)
 admin.site.register(Invoice)
 
-
+#Admin Header
+admin.site.site_header = "Boiled-Youtiao Administration"
